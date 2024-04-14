b090e88343e8a0a896128e0810a151bbf05f0d9b
FTFati <fatima.amkachou@gmail.com>
2024-04-14 07:27:38 -0700

    testt

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Read reddit API and return number subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    datas = response.json().get("data")
+    return datas.get("subscribers")
