14f1c2357a1be13fce7db9ece67913c4c7d843f5
FTFati <fatima.amkachou@gmail.com>
2024-04-20 10:22:39 +0000

    test

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# testing how well our web server setup featuring Nginx is doing under pressure
+# Increase the ULIMIT of the default file
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
